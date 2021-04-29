d407364d569e305a14f8a0776dae7c383d86c116
kamp <nbvdkamp@gmail.com>
2021-04-29 22:25:44 +0200

    Move scene to separate file

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,9 @@ use std::time::Instant;
 use std::path::Path;
 
 mod mesh;
-use mesh::{Vertex, VertexIndex, VertexSemantics, Mesh};
+use mesh::VertexSemantics;
+mod scene;
+use scene::Scene;
 
 fn main() {
     let dim = WindowDim::Windowed {
@@ -33,65 +35,6 @@ fn main() {
         Err(e) => {
             eprintln!("Could not create graphics surface:\n{}", e);
             exit(1);
-        }
-    }
-}
-
-pub struct Scene {
-    meshes: Vec<Mesh>,
-}
-
-impl Scene {
-    fn load<P>(path: P) -> Result<Self, String>
-    where
-        P: AsRef<Path>,
-    {
-        if let Ok((document, buffers, _)) = gltf::import(path) {
-            let mut meshes = Vec::<Mesh>::new();
-            
-            for scene in document.scenes() {
-                for node in scene.nodes() {
-                    if let Some(mesh) = node.mesh() {
-                        for primitive in mesh.primitives() {
-                            let reader = primitive.reader(|buffer| Some(&buffers[buffer.index()]));
-
-                            let positions = {
-                                let iter = reader
-                                    .read_positions()
-                                    .unwrap_or_else(||
-                                        panic!("Primitive does not have POSITION attribute (mesh: {}, primitive: {})", mesh.index(), primitive.index())
-                                    );
-                                iter.collect::<Vec<_>>()
-                            };
-
-                            let vertices: Vec<Vertex> = positions
-                                .into_iter()
-                                .map(|position| {
-                                    Vertex {
-                                        position: position.into()
-                                    }
-                                }).collect();
-                            
-                            let indices: Vec<VertexIndex> = reader
-                                .read_indices()
-                                .map(|read_indices| {
-                                    read_indices.into_u32().collect::<Vec<_>>()
-                                })
-                                .unwrap_or_else(||
-                                    panic!("Primitive has no indices (mesh: {}, primitive: {})", mesh.index(), primitive.index())
-                                );
-                            
-                            //TODO get normals and stuff.
-                            meshes.push(Mesh { vertices, indices });
-                        }
-                    }
-                }
-            }
-
-            Ok(Scene { meshes })
-        }
-        else {
-            Err("Couldn't open glTF file.".into())
         }
     }
 }
