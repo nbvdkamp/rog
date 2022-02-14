c9403e7f367cf13bc1c68113b83873333d6d510e
kamp <nbvdkamp@gmail.com>
2022-02-14 18:06:58 +0100

    Add from_hex

--- src/color.rs
+++ src/color.rs
@@ -22,6 +22,13 @@ macro_rules! impl_f32_color_tuple {
             pub fn $field_normalized(&self) -> u8 {
                 (self.$field * 255.0) as u8
             })+
+
+            pub fn from_hex(hex: &str) -> Self {
+                let h = hex.trim_start_matches("#");
+                let mut i = 0;
+                $(let $field = i32::from_str_radix(&h[(i * 2)..((i + 1) * 2)], 16).unwrap(); i += 1;)+
+                Self::new($($field as f32 / 255.0),+)
+            }
         }
 
         impl_op_ex!(+ |a: &$name, b: &$name| -> $name { $name::new($(a.$field + b.$field),+)} );
@@ -60,6 +67,8 @@ impl From<[f32; 3]> for RGBf32 {
 
 #[cfg(test)]
 mod tests {
+    use cgmath::assert_abs_diff_eq;
+
     use super::*;
 
     #[test]
@@ -79,6 +88,14 @@ mod tests {
     fn add_assign() {
         let mut c = RGBf32::new(0.1, 0.2, 0.5);
         c += RGBf32::new(0.1, 0.1, 0.1);
-        assert_eq!(c * 2.0, RGBf32::new(0.2, 0.4, 1.0));
+        assert_eq!(c, RGBf32::new(0.2, 0.3, 0.6));
+    }
+
+    #[test]
+    fn from_hex() {
+        let c = RGBf32::from_hex("#4080C0");
+        assert_abs_diff_eq!(c.r, 0.25, epsilon = 0.01);
+        assert_abs_diff_eq!(c.g, 0.5, epsilon = 0.01);
+        assert_abs_diff_eq!(c.b, 0.75, epsilon = 0.01);
     }
 }
