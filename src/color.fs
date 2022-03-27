7d60383830eaf2a21e80554bf9893d25003152ea
kamp <nbvdkamp@gmail.com>
2022-03-27 15:25:05 +0200

    Reexport test scene with z up and change preview shader light dir

--- src/color.fs
+++ src/color.fs
@@ -4,6 +4,6 @@ in vec3 v_normal;
 in vec4 v_base_color;
 
 void main() {
-    const vec3 light_dir = vec3(0., -1., -.5);
+    const vec3 light_dir = vec3(0., 1., -.5);
     frag_color = v_base_color * dot(v_normal, -normalize(light_dir));
 }
