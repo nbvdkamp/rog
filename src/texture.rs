e035db2824ddf3e738a44fe62c9807d46c231772
kamp <nbvdkamp@gmail.com>
2022-03-30 01:05:09 +0200

    Fix texture sampling out of bounds

--- src/texture.rs
+++ src/texture.rs
@@ -2,6 +2,7 @@ use image::RgbaImage;
 
 use crate::color::RGBf32;
 
+#[derive(Clone)]
 pub struct Texture {
     image: RgbaImage,
 }
@@ -14,8 +15,8 @@ impl Texture {
     pub fn sample(&self, u: f32, v: f32) -> RGBf32 {
         let (width, height) = self.image.dimensions();
         // TODO: Bilinear interpolation
-        let x = (u * width as f32).round() as u32;
-        let y = (v * height as f32).round() as u32;
+        let x = (u * (width - 1) as f32).round() as u32 % width;
+        let y = (v * (height - 1) as f32).round() as u32 % height;
         let p = self.image.get_pixel(x, y);
         RGBf32::new(p[0] as f32 / 255.0, p[1] as f32 / 255.0, p[2] as f32 / 255.0)
     }
