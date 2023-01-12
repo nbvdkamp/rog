96fa1b1bb16789e7718e55183b22ab1b5ead776e
kamp <nbvdkamp@gmail.com>
2023-01-12 17:44:22 +0100

    Improve Moller-Trumbore implementation
    It now more closely matches the original paper, is slightly faster
    because of the early return before computing the inverse determinant and
    lets a bit less light leak through seams between triangles.

--- src/raytracer/ray.rs
+++ src/raytracer/ray.rs
@@ -12,35 +12,38 @@ pub enum IntersectionResult {
 }
 
 impl Ray {
-    // Möller–Trumbore intersection algorithm:
-    pub fn intersect_triangle(&self, p1: Point3<f32>, p2: Point3<f32>, p3: Point3<f32>) -> IntersectionResult {
-        let edge1 = p2 - p1;
-        let edge2 = p3 - p1;
+    // Möller–Trumbore intersection algorithm, without culling backfacing triangles
+    pub fn intersect_triangle(&self, p0: Point3<f32>, p1: Point3<f32>, p2: Point3<f32>) -> IntersectionResult {
+        let edge1 = p1 - p0;
+        let edge2 = p2 - p0;
 
-        let h = self.direction.cross(edge2);
-        let a = edge1.dot(h);
+        let p = self.direction.cross(edge2);
+        let determinant = edge1.dot(p);
 
-        let f = 1.0 / a;
-        let s = self.origin - p1;
-        let u = f * s.dot(h);
+        const EPSILON: f32 = 1e-6;
 
-        const EPSILON: f32 = 2.0e-5;
-
-        // Wider range to prevent rays from passing through seams between triangles
-        if u < -EPSILON || u > 1.0 + EPSILON {
+        if determinant > -EPSILON && determinant < EPSILON {
             return IntersectionResult::Miss;
         }
 
-        let q = s.cross(edge1);
-        let v = f * self.direction.dot(q);
+        let inverse_determinant = 1.0 / determinant;
+        let t = self.origin - p0;
+        let u = inverse_determinant * t.dot(p);
 
-        if v < -EPSILON || u + v > 1.0 + EPSILON {
+        if u < 0.0 || u > 1.0 {
             return IntersectionResult::Miss;
         }
 
-        let t = f * edge2.dot(q);
+        let q = t.cross(edge1);
+        let v = inverse_determinant * self.direction.dot(q);
 
-        if t > EPSILON {
+        if v < 0.0 || u + v > 1.0 {
+            return IntersectionResult::Miss;
+        }
+
+        let t = inverse_determinant * edge2.dot(q);
+
+        if t > 0.0 {
             IntersectionResult::Hit { t, u, v }
         } else {
             IntersectionResult::Miss
