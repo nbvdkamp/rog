30288813cae894ee5904b64655e3e9cd260e3d75
kamp <nbvdkamp@gmail.com>
2023-03-23 22:20:30 +0100

    Correct sRGB gamma functions

--- src/color.rs
+++ src/color.rs
@@ -214,7 +214,7 @@ pub const SRGB8_TO_LINEAR_TABLE: [f32; 256] = [
 impl RGBf32 {
     pub fn linear_to_srgb(&self) -> Self {
         let m = |c: f32| {
-            if c < 0.0031308 {
+            if c <= 0.0031308 {
                 if c < 0.0 {
                     0.0
                 } else {
@@ -234,7 +234,7 @@ impl RGBf32 {
 
     pub fn srgb_to_linear(&self) -> Self {
         let m = |c| {
-            if c < 0.04045 {
+            if c <= 0.04045 {
                 if c < 0.0 {
                     0.0
                 } else {
