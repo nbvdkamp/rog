ee32872add84e2edb2de4a914516cd64980fc4da
kamp <nbvdkamp@gmail.com>
2023-07-25 15:54:13 +0300

    diff_images: output error images as PNG with Viridis color map

--- src/diff_images/diff_images.rs
+++ src/diff_images/diff_images.rs
@@ -1,5 +1,13 @@
 mod structural_similarity;
-use renderer::raytracer::{single_channel_image::SingleChannelImage, working_image::WorkingImage};
+use std::{fs::create_dir_all, path::PathBuf};
+
+use cgmath::vec2;
+use itertools::Itertools;
+use renderer::{
+    color::RGBu8,
+    raytracer::{single_channel_image::SingleChannelImage, working_image::WorkingImage},
+    util::save_u8_image,
+};
 use structural_similarity::structural_similarity;
 
 fn main() {
@@ -10,7 +18,9 @@ fn main() {
         std::process::exit(-1);
     }
 
-    let image = match WorkingImage::read_from_file(&args[1], &None) {
+    let path = &args[1];
+
+    let image = match WorkingImage::read_from_file(path, &None) {
         Ok(image) => image,
         Err(e) => {
             eprintln!("Error while opening image: {e}");
@@ -33,18 +43,49 @@ fn main() {
         std::process::exit(-1);
     }
 
-    output_error("MSE", image.mean_square_error(&reference));
-    output_error("rgb_MSE", image.rgb_mean_square_error(&reference));
-    output_error("relMSE", image.relative_mean_square_error(&reference));
+    output_error("MSE", image.mean_square_error(&reference), path);
+    output_error("rgb_MSE", image.rgb_mean_square_error(&reference), path);
+    output_error("relMSE", image.relative_mean_square_error(&reference), path);
 
     let img_grayscale = image.to_grayscale();
     let ref_grayscale = reference.to_grayscale();
     let grayscale_diff = &img_grayscale - &ref_grayscale;
-    output_error("grayscale_MSE", &grayscale_diff * &grayscale_diff);
-    output_error("MSSIM", structural_similarity(img_grayscale, ref_grayscale));
+    output_error("grayscale_MSE", &grayscale_diff * &grayscale_diff, path);
+    output_error("MSSIM", structural_similarity(img_grayscale, ref_grayscale), path);
 }
 
-fn output_error(name: &str, error_image: SingleChannelImage) {
+fn output_error<P>(name: &str, error_image: SingleChannelImage, path: P)
+where
+    PathBuf: From<P>,
+{
     let mean = error_image.mean();
     println!("{name} {mean}");
+
+    let image_size = vec2(error_image.width, error_image.height);
+
+    let pixels = error_image
+        .data
+        .into_iter()
+        .map(|v| {
+            let c = colorous::VIRIDIS.eval_continuous(v.clamp(0.0, 1.0) as f64);
+            RGBu8::new(c.r, c.g, c.b)
+        })
+        .collect_vec();
+
+    let mut path = PathBuf::from(path);
+    let mut file_name = path.file_stem().unwrap().to_owned();
+    file_name.push(".png");
+    path.pop();
+    path.push("diff/");
+    path.push(format!("{name}/"));
+    path.push(file_name);
+
+    match create_dir_all(path.parent().unwrap()) {
+        Ok(_) => (),
+        Err(e) => {
+            eprintln!("Can't create directories for path: {path:?}, {e}");
+            std::process::exit(-1);
+        }
+    }
+    save_u8_image(&pixels, image_size, path, false);
 }
